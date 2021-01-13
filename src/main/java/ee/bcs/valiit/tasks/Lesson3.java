0471388ee1e564f117b27e956ad8c012866a8b51
Annika Orusalu <orusalu.annika@gmail.com>
2021-01-13 15:59:08 +0200

    push1

--- src/main/java/ee/bcs/valiit/tasks/Lesson3.java
+++ src/main/java/ee/bcs/valiit/tasks/Lesson3.java
@@ -2,12 +2,18 @@ package ee.bcs.valiit.tasks;
 
 public class Lesson3 {
     public static void main(String[] args) {
+        //System.out.println(sum();
 
     }
 
     public static int sum(int[] x){
         // Todo liida kokku kõik numbrid massivis x
-        return 0;
+
+        int sum=0; //elemendi indeksid hakkavad 0-ist
+        for (int i=1; i< x.length; i++) { //massiivi pikkus .lengh
+            sum +=i; //liidame summale x väärtuse
+        }
+        return sum;
     }
 
     public static int factorial(int x) {
@@ -15,12 +21,16 @@ public class Lesson3 {
         // Näiteks
         // x = 5
         // return 5*4*3*2*1 = 120
-        return 0;
+        if(x==0) //
+            return 1;
+        else
+            return(x * factorial(x-1));
     }
 
     public static int[] sort(int[] a) {
         // TODO sorteeri massiiv suuruse järgi.
         // TODO kasuta tsükleid, ära kasuta ühtegi olemasolevat sort funktsiooni
+        int arrayList;
         return new int[0];
     }
 
